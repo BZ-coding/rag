dec378b36ee6c37c10435942fede9368c863e4af
zhangsheng377 <435878393@qq.com>
2024-06-19 00:28:25 +0800

    优化代码

--- main.py
+++ main.py
@@ -12,26 +12,32 @@ from utils.rerankers import Reranker
 from utils.rag import Rag
 
 EMBEDDING_MODEL_PATH = "/mnt/nfs/zsd_server/models/huggingface/embedding_models/BAAI/bge-large-zh-v1.5/"
-TEXT_EMBEDDING_PAIRS_PATH = 'text_embedding_pairs_BAAI_1.pkl'
+TEXT_EMBEDDING_PAIRS_PATH = 'text_embedding_pairs_BAAI.pkl'
 RERANKER_MODEL_PATH = "/mnt/nfs/zsd_server/models/huggingface/reranker_models/BAAI/bge-reranker-large/"
 # CHAT_MODEL_PATH = "/mnt/nfs/zsd_server/models/huggingface/chinese-alpaca-2-7b/"
 CHAT_MODEL_PATH = "/mnt/nfs/zsd_server/models/huggingface/llama-3-chinese-8b-instruct-v3/"
+
+
+def answer(vectorstore, chat_model, tokenizer, query, ranker_model_path=None):
+    if ranker_model_path:
+        retriever = vectorstore.get_retriever(search_kwargs={"k": 20})
+        reranker = Reranker(ranker_model_path=RERANKER_MODEL_PATH, retriever=retriever, topn=3)
+        retriever = reranker.as_retriever()
+    else:
+        retriever = vectorstore.get_retriever(search_kwargs={"k": 3})
+    rag = Rag(chat_model=chat_model, tokenizer=tokenizer, retriever=retriever)
+    rag.answer(query=query)
+
 
 if __name__ == '__main__':
     with open("刑法.txt", "r") as f:
         data = f.readlines()
     data = [d.strip() for d in data]
-    # print(f"data sample : {data[:2]}")
-    # print(f"data length : {len(data)}")
 
     embedding_model = get_embedding_model(model_path=EMBEDDING_MODEL_PATH)
-    # print(f"embedding model : {embedding_model}")
-
     text_embedding_pairs = get_text_embedding_pairs(embedding_model=embedding_model, data=data,
                                                     text_embedding_pairs_path=TEXT_EMBEDDING_PAIRS_PATH)
-
-    vectorstore = FaissVectorStore(text_embedding_pairs=text_embedding_pairs, embedding_model=embedding_model,
-                                   search_kwargs={"k": 3})
+    vectorstore = FaissVectorStore(text_embedding_pairs=text_embedding_pairs, embedding_model=embedding_model)
 
     chat_model = AutoModelForCausalLM.from_pretrained(
         CHAT_MODEL_PATH,
@@ -41,16 +47,9 @@ if __name__ == '__main__':
     )
     tokenizer = AutoTokenizer.from_pretrained(CHAT_MODEL_PATH)
 
-    rag = Rag(chat_model=chat_model, tokenizer=tokenizer, retriever=vectorstore.retriever)
-
     query = "持有管制刀具怎么判？"
-    rag.answer(query=query)
+    answer(vectorstore=vectorstore, chat_model=chat_model, tokenizer=tokenizer, query=query)
 
     print("\n\n\n")
-    text_embedding_pairs = get_text_embedding_pairs(embedding_model=embedding_model, data=data,
-                                                    text_embedding_pairs_path=TEXT_EMBEDDING_PAIRS_PATH)
-    vectorstore = FaissVectorStore(text_embedding_pairs=text_embedding_pairs, embedding_model=embedding_model,
-                                   search_kwargs={"k": 20})
-    reranker = Reranker(ranker_model_path=RERANKER_MODEL_PATH, retriever=vectorstore.retriever, topn=3)
-    rag = Rag(chat_model=chat_model, tokenizer=tokenizer, retriever=reranker.as_retriever())
-    rag.answer(query=query)
+    answer(vectorstore=vectorstore, chat_model=chat_model, tokenizer=tokenizer, query=query,
+           ranker_model_path=RERANKER_MODEL_PATH)
--- utils/vectorstores.py
+++ utils/vectorstores.py
@@ -2,12 +2,13 @@ from langchain_community.vectorstores import FAISS
 
 
 class FaissVectorStore:
-    def __init__(self, text_embedding_pairs, embedding_model, search_kwargs=None):
+    def __init__(self, text_embedding_pairs, embedding_model):
         self.faiss = FAISS.from_embeddings(text_embedding_pairs, embedding_model)
-
-        if search_kwargs is None:
-            search_kwargs = {}
-        self.retriever = self.faiss.as_retriever(search_kwargs=search_kwargs)
 
     def similarity_search(self, query):
         return self.faiss.similarity_search(query)
+
+    def get_retriever(self, search_kwargs=None):
+        if search_kwargs is None:
+            search_kwargs = {}
+        return self.faiss.as_retriever(search_kwargs=search_kwargs)
