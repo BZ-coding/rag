7d9bce5c5a31272e168bfdd80a504a28e00c646d
zhangsheng377 <435878393@qq.com>
2024-07-08 00:25:25 +0800

    测试ollama生成的兼容openai格式的api

---
+++ experiments/test_openai_compatibility_api.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url='http://localhost:11434/v1/',
+    api_key='ollama',  # required but ignored
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            'role': 'user',
+            'content': 'Say this is a test',
+        }
+    ],
+    model='llama-3-chinese-8b-instruct-v3-f16',
+)
+
+print(chat_completion)
+print(chat_completion.choices[0].message.content)
